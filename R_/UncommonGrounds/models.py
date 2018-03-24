3a8c292abb732277934531b052c81ca5591f4722
EEmmons <eemmons@umass.edu>
2018-03-24 16:31:06 -0400

    added comment model

--- R_/UncommonGrounds/models.py
+++ R_/UncommonGrounds/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Tag(models.Model):
@@ -23,6 +24,7 @@ class Location(models.Model):
     ratings = models.FloatField()
     popularity = models.IntegerField()
     comments = models.TextField(max_length=1000)
+    # comments = models.ForeignKey('Comment', on_delete=models.SET_NULL, null=True) 
     image = models.ImageField(upload_to = 'location_images/')
 
     def __str__(self):
@@ -47,3 +49,26 @@ class User(models.Model):
         String for representing the Model object (in Admin site etc.)
         """
         return self.username
+
+
+class Comment(models.Model):
+    """
+    Model representing a comment.
+    """
+    author = models.ForeignKey('User', on_delete=models.SET_NULL, null=True) 
+    text = models.TextField(max_length=500)
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+        """
+        This will be used to moderate comments and make sure only valid users can comment
+        """
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.text
