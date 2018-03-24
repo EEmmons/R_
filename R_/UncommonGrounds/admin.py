e801d841789c5b04fa6fb78786abff3cef1ba1db
EEmmons <eemmons@umass.edu>
2018-03-24 16:37:59 -0400

    regitered comment model

--- R_/UncommonGrounds/admin.py
+++ R_/UncommonGrounds/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Location, Tag
+from .models import User, Location, Tag, Comment
 
 admin.site.register(User)
 admin.site.register(Location)
 admin.site.register(Tag)
+admin.site.register(Comment)
