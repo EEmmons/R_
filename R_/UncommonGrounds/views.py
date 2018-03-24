d1c71bfdd99fc16bf5eb6d2fa1b081505e17b5e3
EEmmons <eemmons@umass.edu>
2018-03-24 16:36:45 -0400

    added comment list view and imported models

--- R_/UncommonGrounds/views.py
+++ R_/UncommonGrounds/views.py
@@ -1,38 +1,40 @@
 from django.shortcuts import render
-# from .models import UNCOMMON GROUNDS MODELS
+from .models import Location, User, Tag, Comment
 from django.views import generic
 
 # Create your views here.
 
 
-# def index(request):
-#     """
-#     View function for home page of site.
-#     """
-#
-#     # Generate counts of the main objects
-#
-#     num_books = Book.objects.all().count()
-#     num_instances = BookInstance.objects.all().count()
-#
-#     # Available books (status = 'a')
-#
-#     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-#     num_authors = Author.objects.count()
-#
-#     # Render the HTML template index.html with the data in the context variable
-#
-#     return render(
-#         request,
-#         'index.html',
-#         context = {'num_books':num_books, 'num_instances':num_instances, 'num_instances_available':num_instances_available, 'num_authors':num_authors},
-#     )
+def index(request):
+    """
+    View function for home page of site.
+    """
 
-class LocationView(generic.ListView):
+    # Generate counts of the main objects
+
+    num_locations = Location.objects.all().count()
+
+    # # Available books (status = 'a')
+
+    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    # num_authors = Author.objects.count()
+
+    # Render the HTML template index.html with the data in the context variable
+
+    return render(
+        request,
+        'index.html',
+        context = {'num_locations':num_locations}
+    )
+
+class LocationListView(generic.ListView):
     model = Location
 
-class UserView(generic.ListView):
+class UserListView(generic.ListView):
     model = User
 
-class TagView(generic.ListView):
+class TagListView(generic.ListView):
     model = Tag
+
+class CommentListView(generic.ListView):
+	model = Comment
