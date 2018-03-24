ea6bfd220aa1d86dceed1a48484ebd4f356a56a7
EEmmons <eemmons@umass.edu>
2018-03-24 16:39:35 -0400

    added views path functions for models

--- R_/UncommonGrounds/urls.py
+++ R_/UncommonGrounds/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
 
 	path('', views.index, name='index'),
-
+	path('locations/', views.LocationListView.as_view(), name='locations'),
+	path('users/', views.UserListView.as_view(), name='users'),
+	path('tags/', views.TagListView.as_view(), name='tags'),
+	path('comments/', views.CommentListView.as_view(), name='comments'),
 ]
