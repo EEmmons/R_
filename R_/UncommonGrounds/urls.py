896d9ce9335f1cc890ba30e2ddaaff6a91ff8617
TCK <tckearns@umass.edu>
2018-03-26 13:45:06 -0400

    templates

--- R_/UncommonGrounds/urls.py
+++ R_/UncommonGrounds/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
 	path('', views.discover, name='discover'),
 	path('locations/', views.LocationListView.as_view(), name='locations'),
-	path('locations/<int:pk>', views.LocationDetailView.as_view(), name='location-detail'),
+	path('location/<int:pk>', views.LocationDetailView.as_view(), name='location-detail'),
 	path('users/', views.UserListView.as_view(), name='users'),
 	path('user/<int:pk>', views.UserDetailView.as_view(), name='user-detail'),
 	path('tags/', views.TagListView.as_view(), name='tags'),
