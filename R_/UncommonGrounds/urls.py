2c15930e69cadbe1882cae56a1b10aacdbbeda79
TCK <tckearns@umass.edu>
2018-03-29 19:46:18 -0400

    stuff

--- R_/UncommonGrounds/urls.py
+++ R_/UncommonGrounds/urls.py
@@ -4,14 +4,11 @@ from . import views
 
 urlpatterns = [
 
-<<<<<<< HEAD
 	path('', views.home, name='home'),
 	path('discover/', views.discover, name='discover'),
-=======
 	path('', views.discover, name='discover'),
 	path('profile/', views.profile, name='profile'),
 	path('addLocation/', views.addLocation, name='addLocation'),
->>>>>>> ffdf6f0fbe1ca5a71fad144869013e84feaa9b50
 	path('locations/', views.LocationListView.as_view(), name='locations'),
 	path('location/<int:pk>', views.LocationDetailView.as_view(), name='location-detail'),
 	path('users/', views.UserListView.as_view(), name='users'),
